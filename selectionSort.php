7f3541e1154b1529dd4eaa79927ffa77777158be
chenminfeng <chenminfeng@yaochufa.com>
2016-07-25 10:16:02 +0800

    selectionSort

---
+++ selectionSort.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenminfeng
+ * Date: 2016/7/25
+ * Time: 9:56
+ */
+
+function selectionSort($arr){
+    $n = count($arr);
+    for($i = 0; $i < $n; $i++){
+        $minIndex = $i;
+        for($j = $i; $j < $n; $j ++){
+            if($arr[$j] < $arr[$minIndex]){
+                $minIndex = $j;
+            }
+        }
+        $tmp = $arr[$minIndex];
+        $arr[$minIndex] = $arr[$i];
+        $arr[$i] = $tmp;
+    }
+    return $arr;
+}
+
+$arr = [40,81,25,38,85,31,62,21,43,79];
+$n = count($arr);
+$arr = selectionSort($arr);
+
+for($i = 0; $i < $n; $i++){
+    echo $arr[$i] . ' ';
+}
